bb05d7e8538822765271d4817ed1ce34a7e720e9
hadienzz <hadinpramiadi@gmail.com>
2024-12-09 21:44:13 +0700

    add detailpage

--- app/meals/[mealsSlug]/page.jsx
+++ app/meals/[mealsSlug]/page.jsx
@@ -1,12 +1,35 @@
-import { usePathname } from "next/navigation";
+import { getMeal } from "@/lib/meals";
+import { notFound, usePathname } from "next/navigation";
+import classes from "./page.module.css";
+import Image from "next/image";
 
-const MealsDetailPage = ({ params }) => {
+const MealsDetailPage = async ({ params }) => {
   const { mealsSlug } = params;
+  const meal = getMeal(mealsSlug);
 
+  if (!meal) {
+    notFound();
+  }
+  
   return (
-    <div>
-      <h1>This is {mealsSlug} page</h1>
-    </div>
+    <>
+      <div className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href="mailto:mail">{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </div>
+
+      <pre className={classes.instructions}>
+        <p>{meal.instructions}</p>
+      </pre>
+    </>
   );
 };
 
--- lib/meals.jsx
+++ lib/meals.jsx
@@ -2,7 +2,10 @@ import sql from "better-sqlite3";
 
 const db = sql("meals.db");
 
-export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+export function getMeals() {
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
